5ce536ea8f8ee82ad9e32f0eb7301aa2eb7cd5a7
Ante-237 <akumawahanye@gmail.com>
2023-04-07 22:02:56 +0200

    status for various objects.

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.url_map.strict_slashes = False
 
+host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+port = os.getenv("HBNB_API_PORT", 5000)
+
 
 @app.teardown_appcontext
 def teardown_storage(exception):
@@ -26,6 +29,4 @@ def error(self):
 
 if __name__ == "__main__":
     """ just another flask app """
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port)
