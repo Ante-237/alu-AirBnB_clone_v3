a67831ad46ec3ad9c935aacbd70f59ee03b0af56
Ante-237 <akumawahanye@gmail.com>
2023-04-07 15:23:46 +0200

    first touch api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -15,7 +17,9 @@ def teardown_storage(exception):
     """ closing storage """
     storage.close()
 
+
 if __name__ == "__main__":
+    """ just another flask app """
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def get_status():
+    """ RETURN JSON"""
     return jsonify({'status': 'OK'})
