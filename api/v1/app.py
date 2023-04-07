2f0c4bb0616f91899e1a1c0927cc8111a8f1861b
Ante-237 <akumawahanye@gmail.com>
2023-04-07 22:09:38 +0200

    http access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,14 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.url_map.strict_slashes = False
+CORS(app, resources=r"/api/v1/*", origins="*")
+
 
 host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 port = os.getenv("HBNB_API_PORT", 5000)
