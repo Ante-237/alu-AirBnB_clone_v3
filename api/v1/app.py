fccdb55f555680c25f3c5151b30b0612c1e38e2a
Ante-237 <akumawahanye@gmail.com>
2023-04-07 21:34:44 +0200

    changes to context formating.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ creating default route """
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 import os
@@ -18,8 +18,14 @@ def teardown_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def error(self):
+    """404 error but return empty dict"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     """ just another flask app """
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
