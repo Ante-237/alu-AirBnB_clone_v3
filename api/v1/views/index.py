41a91d3e60ad69980e9468d8894d370ffb1d588a
Ante-237 <akumawahanye@gmail.com>
2023-04-25 22:22:55 +0200

    changes to stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,23 +5,23 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
+from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET']))
 def get_status():
     """ RETURN JSON"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def get_count():
     """ count number state """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
     classes = {
         Amenity: "amenities",
         City: "cities",
